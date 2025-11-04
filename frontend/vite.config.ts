db26e24b1c7aee53c540bf66e32384b05f7512c2
bodjernisschroder <bdsc63266@edu.ucl.dk>
2025-11-04 11:56:41 +0100

    Fixed problem causing the program not to run

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,32 @@
-// vite.config.js
-import { defineConfig } from 'vitest/config';
+// // vite.config.js
+// import { defineConfig } from 'vitest/config';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite';
+// import { fileURLToPath } from 'node:url';
+// import { dirname, resolve } from 'node:path';
+
+// // ESM-safe __dirname
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, 'src'),
+//     },
+//   },
+  
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: './src/setupTests.js'
+//   }
+// });
+
+// vite.config.ts
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'node:path';
 
@@ -10,16 +35,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: { postcss: './postcss.config.js' },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
   },
-  
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
-  }
+    setupFiles: './src/setupTests.js',
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
 });
